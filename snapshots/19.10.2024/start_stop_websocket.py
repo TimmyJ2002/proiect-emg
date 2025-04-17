2e5369d29fc6d19de1a9f12738e7fd995d9c364b
TimmyJ2002 <timonj2002@gmail.com>
2025-04-17 19:52:59 +0300

    Snapshot - 19.10.2024

---
+++ snapshots/19.10.2024/start_stop_websocket.py
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="0">
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="1019" startPos="427" endPos="427" selMode="0" offset="0" wrapCount="1" lang="Python" encoding="-1" userReadOnly="no" filename="C:\Users\janost\Desktop\uni\proiect-emg\start_stop_websocket.py" backupFilePath="" originalFileLastModifTimestamp="67211848" originalFileLastModifTimestampHigh="31136566" tabColourId="-1" RTL="no" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="512" mapWrapIndentMode="-1" mapIsWrap="no" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
